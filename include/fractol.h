0a256f7e0acd6f7f611aea7225d8809d6d465be9
Okamoto Yayoi <oyayoi@c2r5s6.42tokyo.jp>
2025-05-24 18:00:55 +0900

    add_key

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: oyayoi <oyayoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:12:53 by okamotoyayo       #+#    #+#             */
-/*   Updated: 2025/05/23 16:53:00 by oyayoi           ###   ########.fr       */
+/*   Updated: 2025/05/24 16:39:04 by oyayoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,19 @@ typedef struct s_data
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	void	*mlx;
+	void	*mlx_win;
 }	t_data;
+
+# define WIDTH 1920
+# define HEIGHT 1080
+# define MAX_ITER 100
+
+# define ESC_KEY 0xFF1B
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 int		calc_mandelbrot(double x0, double y0);
+int		close_window(t_data *param);
+int		key_event(int keycode, t_data *param);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oyayoi <oyayoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 14:56:07 by okamotoyayo       #+#    #+#             */
-/*   Updated: 2025/05/23 17:38:04 by oyayoi           ###   ########.fr       */
+/*   Updated: 2025/05/24 16:44:27 by oyayoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,36 +22,49 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
 	int		i;
 	int		j;
-	double  scale;
-	int 	color;
+	double	scale;
+	int		color;
 
 	i = 0.0;
 	j = 0.0;
 	scale = 0.004;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.mlx = mlx_init();
+	img.mlx_win = mlx_new_window(img.mlx, WIDTH, HEIGHT, "Hello world!");
+	img.img = mlx_new_image(img.mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while (i < 1920)
+	while (i < WIDTH)
 	{
 		j = 0;
-		while (j < 1080)
+		while (j < HEIGHT)
 		{
-			color = calc_mandelbrot((i - 1920 / 2) * scale, (j - 1080 / 2) * scale);
+			color = calc_mandelbrot((i - WIDTH / 2) * scale, (j - HEIGHT / 2) * scale);
 			if (color > 0)
 				my_mlx_pixel_put(&img, i, j, 0x00FFFFFF - (color * 0x00FCBE11));
 			j++;
 		}
 		i++;
 	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+	mlx_key_hook(img.mlx_win, key_event, &img);
+	mlx_hook(img.mlx_win, 17, 0, close_window, &img);
+	mlx_loop(img.mlx);
+}
+
+int	key_event(int keycode, t_data *param)
+{
+	if (keycode == ESC_KEY)
+		close_window(param);
+	return (0);
+}
+
+int	close_window(t_data *param)
+{
+	mlx_destroy_window(param->mlx, param->mlx_win);
+	exit(0);
 }
 
 int	calc_mandelbrot(double x0, double y0)
@@ -60,20 +73,18 @@ int	calc_mandelbrot(double x0, double y0)
 	double	y;
 	double	x_temp;
 	int		iter;
-	int		max_iter;
 
 	x = 0.0;
 	y = 0.0;
 	iter = 0;
-	max_iter = 100;
-	while (x * x + y * y <= 2.0 * 2.0 && iter < max_iter)
+	while (x * x + y * y <= 2.0 * 2.0 && iter < MAX_ITER)
 	{
 		x_temp = x * x - y * y + x0;
 		y = 2 * x * y + y0;
 		x = x_temp;
 		iter++;
 	}
-	if (iter < max_iter)
+	if (iter < MAX_ITER)
 		return (iter);
 	else
 		return (0);
